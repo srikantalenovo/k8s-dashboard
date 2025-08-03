ecbebf9144e5035c7aa981d9472a394462ee2d0b
devopsodia <odiadevops@gmail.com>
2025-08-03 23:27:56 +0530

    deploy on k8s cluster

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+ baseURL: process.env.REACT_APP_API_URL || 'http://grepmind-backend:5000/api',
  timeout: 10000,
  headers: {
    'Content-Type': 'application/json',
