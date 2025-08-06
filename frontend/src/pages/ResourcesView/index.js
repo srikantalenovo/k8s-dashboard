ff649197c0cb660f4b2dd2cf800c64d0e700f238
devopsodia <odiadevops@gmail.com>
2025-08-06 14:33:44 +0530

    Created files for Analyzerview

--- frontend/src/pages/ResourcesView/index.js
+++ frontend/src/pages/ResourcesView/index.js
@@ -1,5 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
+import api from '../../services/api'
+
 
 import {
   Box, Typography, Select, MenuItem, Table, TableBody, Tooltip, FormControl, InputLabel, Alert,
